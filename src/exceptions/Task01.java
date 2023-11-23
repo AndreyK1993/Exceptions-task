5765005957a35991044ea0b346113103015a0b8f
AndreyK1993 <andrey.kurilenko27@gmail.com>
2023-11-23 23:15:34 +0200

    Add task 1

--- src/exceptions/Task01.java
+++ src/exceptions/Task01.java
@@ -1,4 +1,31 @@
 package exceptions;
 
+import java.util.Scanner;
+
 public class Task01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter number: ");
+            String input = scanner.nextLine();
+
+            // Попытка преобразования введенной строки в число
+            int number = Integer.parseInt(input);
+
+            // Вывод результата
+            System.out.println("Number is: " + number);
+
+        } catch (NumberFormatException nfe) {
+            // Обробка винятку
+            System.out.println("Exception: " + nfe);
+        }
+        finally {
+            scanner.close();
+        }
+
+        System.out.println("Further down the code...");
+    }
 }
